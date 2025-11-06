b20c90bd1a1125c029a538b573326303c3173c02
Abrarul Rhythm <[....email>
2025-11-07 00:41:57 +0600

    added response interceptor

--- src/Pages/MyBids/MyBids.jsx
+++ src/Pages/MyBids/MyBids.jsx
@@ -1,24 +1,19 @@
 import React, { use, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import TableRow from '../../components/TableRow/TableRow';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const MyBids = () => {
     const { user } = use(AuthContext);
     const [bids, setBids] = useState([]);
+    const axiosSecure = useAxiosSecure();
 
     useEffect(() => {
-        if (user?.email) {
-            fetch(`http://localhost:3000/bids?email=${user.email}`, {
-                headers: {
-                    authorization: `Bearer ${user.accessToken}`
-                }
+        axiosSecure.get(`/bids?email=${user.email}`)
+            .then(data => {
+                setBids(data.data);
             })
-                .then(res => res.json())
-                .then(data => {
-                    setBids(data);
-                })
-        }
-    }, [user]);
+    }, [user, axiosSecure]);
 
     return (
         <section className='my-bids py-11 lg:py-20'>
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,18 +1,56 @@
 import axios from "axios";
 import useAuth from "./useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000',
 });
 
 const useAxiosSecure = () => {
-    const { user } = useAuth();
+    const { user, signOutUser } = useAuth();
+    const navigate = useNavigate();
 
-    // set token in the header for all the api call using axiosSecure hook
-    axiosInstance.interceptors.request.use((config) => {
-        config.headers.authorization = `Bearer ${user.accessToken}`;
-        return config;
-    })
+    useEffect(() => {
+
+        // request interceptor
+        const requestInterceptor = axiosInstance.interceptors.request.use((config) => {
+            if (user?.accessToken) {
+                config.headers.authorization = `Bearer ${user.accessToken}`;
+            }
+            return config;
+        })
+
+        // response interceptor
+        const responseInterceptor = axiosInstance.interceptors.response.use(res => {
+            return res;
+        }, err => {
+            const status = err.status;
+            if (status === 401 || status === 403) {
+                signOutUser()
+                    .then(() => {
+                        // navigate user to the login page
+                        navigate('/sign-in');
+                        Swal.fire({
+                            icon: "error",
+                            title: "Oops...",
+                            text: "Access denied. Please log in again.",
+                        });
+                    })
+                    .catch((error) => {
+                        toast.error(error.message);
+                    })
+            }
+        })
+
+        return () => {
+            axiosInstance.interceptors.request.eject(requestInterceptor);
+            axiosInstance.interceptors.response.eject(responseInterceptor);
+        }
+
+    }, [user, signOutUser, navigate])
 
     return axiosInstance;
 }
